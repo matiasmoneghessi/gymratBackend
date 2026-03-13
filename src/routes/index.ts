1506923a720cbbb436d51dc6f613c96faaca5fc9
Matias Moneghessi <matimone9@gmail.com>
2026-03-13 20:37:33 -0300

    feat: add usuario and rutina routes to the main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,16 @@ import semanaRoutes from './semana.routes';
 import diaRoutes from './dia.routes';
 import ejercicioRoutes from './ejercicio.routes';
 import ejercicioSemanaRoutes from './ejercicioSemana.routes';
+import usuarioRoutes from './usuario.routes';
+import rutinaRoutes from './rutina.routes';
 
 const router = Router();
 
+router.use('/rutinas', rutinaRoutes);
 router.use('/semanas', semanaRoutes);
 router.use('/dias', diaRoutes);
 router.use('/ejercicios', ejercicioRoutes);
 router.use('/ejercicio-semana', ejercicioSemanaRoutes);
+router.use('/usuarios', usuarioRoutes);
 
 export default router;
